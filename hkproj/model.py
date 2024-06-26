68a41867b4008b4b7aeb8680a16208b00a2f0a38
Lorenzo Ramírez Mejía <e0473221@student.tuwien.ac.at>
2024-06-26 21:09:47 +0200

    Add presentation

--- hkproj/model.py
+++ hkproj/model.py
@@ -43,23 +43,6 @@ class PositionalEncoding(nn.Module):
         x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)
         return self.dropout(x)
 
-'''# Rember we use this straight from torch, # TODO: check
-class LayerNormalization(nn.Module):
-
-    def __init__(self, features: int, eps:float=10**-6) -> None:
-        super().__init__()
-        self.eps = eps
-        self.alpha = nn.Parameter(torch.ones(features)) # alpha is a learnable parameter
-        self.bias = nn.Parameter(torch.zeros(features)) # bias is a learnable parameter
-
-    def forward(self, x):
-        # x: (batch, seq_len, hidden_size)
-         # Keep the dimension for broadcasting
-        mean = x.mean(dim = -1, keepdim = True) # (batch, seq_len, 1)
-        # Keep the dimension for broadcasting
-        std = x.std(dim = -1, keepdim = True) # (batch, seq_len, 1)
-        # eps is to prevent dividing by zero or when std is very small
-        return self.alpha * (x - mean) / (std + self.eps) + self.bias'''
     
 class FeedForwardBlock(nn.Module):
     # Paper: linear layer with 2 matrices and relu in between
